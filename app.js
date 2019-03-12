e687ac3c8445c55e873877aca29756773fa8bda2
Your Name <you@example.com>
2019-03-12 08:57:32 +0100

    Initalize

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+var app = express();
+var path = require('path');
+
+function pet(name, age, type){
+    this.name = name;
+    this.age = age;
+    this.type = type;
+}
+
+let data = new pet(
+    "Spot",
+    3,
+    "dog");
+
+app.get('/', (req, res) => {
+ res.sendFile(path.join(__dirname + '/hello.html'))
+});
+
+app.get('/data', (req, res) => {
+    res.json(data)
+   });
+
+app.listen(process.env.PORT || 8080)
